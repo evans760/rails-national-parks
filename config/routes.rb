2fd3942690ee9a8cd2ba48a56f6e7748ef72a63d
Thomas <evans_760@yahoo.com>
2016-07-13 09:45:51 -0700

    anther commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'main#home'
 
   resources :parks
+  resources :rangers
 
 
   # The priority is based upon order of creation: first created -> highest priority.
